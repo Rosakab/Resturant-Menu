bc4ea06ad72f46d5473d69a655398f20de0c66e6
Rosakab <akkabiri2003@gmail.com>
2024-01-06 15:12:53 -0800

    create a one page app for resturant menu

---
+++ src/data.js
@@ -0,0 +1,269 @@
+const menu = [
+  {
+    id: 1,
+    title: "Yummy Desserts",
+    category: "Dessert",
+    price: 9.99,
+    img: "./images/pic1.jpg",
+    desc: `You can find countless different variations on desserts, from key lime cheesecake to oreo cookie milkshakes. `,
+  },
+  {
+    id: 2,
+    title: "Yummy Desserts",
+    category: "Dessert",
+    price: 9.99,
+    img: "./images/pic2.jpg",
+    desc: `You can find countless different variations on desserts, from key lime cheesecake to oreo cookie milkshakes. `,
+  },
+  {
+    id: 3,
+    title: "Yummy Desserts",
+    category: "Dessert",
+    price: 9.99,
+    img: "./images/pic3.jpg",
+    desc: `You can find countless different variations on desserts, from key lime cheesecake to oreo cookie milkshakes.`,
+  },
+  {
+    id: 4,
+    title: "Yummy Desserts",
+    category: "Dessert",
+    price: 9.99,
+    img: "./images/pic4.jpg",
+    desc: `You can find countless different variations on desserts, from key lime cheesecake to oreo cookie milkshakes. `,
+  },
+  {
+    id: 5,
+    title: "Yummy Desserts",
+    category: "Dessert",
+    price: 9.99,
+    img: "./images/pic5.jpg",
+    desc: `You can find countless different variations on desserts, from key lime cheesecake to oreo cookie milkshakes. `,
+  },
+  {
+    id: 6,
+    title: "Yummy Desserts",
+    category: "Dessert",
+    price: 9.99,
+    img: "./images/pic6.jpg",
+    desc: `You can find countless different variations on desserts, from key lime cheesecake to oreo cookie milkshakes.`,
+  },
+  {
+    id: 7,
+    title: "Delicious Appetizers",
+    category: "appetizers",
+    price: 12.99,
+    img: "./images/pic7.jpg",
+    desc: `You can find countless different variations on appetizers. `,
+  },
+  {
+    id: 8,
+    title: "Delicious Appetizers",
+    category: "appetizers",
+    price: 12.99,
+    img: "./images/pic8.jpg",
+    desc: `You can find countless different variations on appetizers.  `,
+  },
+  {
+    id: 9,
+    title: "Delicious Appetizers",
+    category: "appetizers",
+    price: 12.99,
+    img: "./images/pic9.jpg",
+    desc: `You can find countless different variations on appetizers.`,
+  },
+  {
+    id: 10,
+    title: "Delicious Appetizers",
+    category: "appetizers",
+    price: 12.99,
+    img: "./images/pic10.jpg",
+    desc: `You can find countless different variations on appetizers. `,
+  },
+
+  {
+    id: 11,
+    title: "Delicious Appetizers",
+    category: "appetizers",
+    price: 12.99,
+    img: "./images/pic11.jpg",
+    desc: `You can find countless different variations on appetizers. `,
+  },
+
+  {
+    id: 12,
+    title: "Delicious Appetizers",
+    category: "appetizers",
+    price: 12.99,
+    img: "./images/pic12.jpg",
+    desc: `You can find countless different variations on appetizers. `,
+  },
+
+
+  {
+    id: 13,
+    title: "Delicious Main course",
+    category: "Entrée",
+    price: 20.99,
+    img: "./images/pic13.jpg",
+    desc: `You can find countless different variations main course and enjoy eating. `,
+  },
+
+  {
+    id: 14,
+    title: "Delicious Main course",
+    category: "Entrée",
+    price: 20.99,
+    img: "./images/pic14.jpg",
+    desc: `You can find countless different variations main course and enjoy eating. `,
+  },
+  {
+    id: 15,
+    title: "Delicious Main course",
+    category: "Entrée",
+    price: 20.99,
+    img: "./images/pic15.jpg",
+    desc: `You can find countless different variations main course and enjoy eating. `,
+  },
+  {
+    id: 16,
+    title: "Delicious Main course",
+    category: "Entrée",
+    price: 20.99,
+    img: "./images/pic16.jpg",
+    desc: `You can find countless different variations main course and enjoy eating. `,
+  },
+  {
+    id: 17,
+    title: "Delicious Main course",
+    category: "Entrée",
+    price: 20.99,
+    img: "./images/pic17.jpg",
+    desc: `You can find countless different variations main course and enjoy eating. `,
+  },
+  {
+    id: 18,
+    title: "Delicious Main course",
+    category: "Entrée",
+    price: 20.99,
+    img: "./images/pic18.jpg",
+    desc: `You can find countless different variations main course and enjoy eating. `,
+  },
+  {
+    id: 19,
+    title: "Delicious Main course",
+    category: "Entrée",
+    price: 20.99,
+    img: "./images/pic19.jpg",
+    desc: `You can find countless different variations main course and enjoy eating. `,
+  },
+  {
+    id: 20,
+    title: "Delicious Main course",
+    category: "Entrée",
+    price: 20.99,
+    img: "./images/pic20.jpg",
+    desc: `You can find countless different variations main course and enjoy eating. `,
+  },
+  {
+    id: 21,
+    title: "Delicious Main course",
+    category: "Entrée",
+    price: 20.99,
+    img: "./images/pic21.jpg",
+    desc: `You can find countless different variations main course and enjoy eating. `,
+  },
+
+  {
+    id: 22,
+    title: "Delicious Main course",
+    category: "Entrée",
+    price: 20.99,
+    img: "./images/pic22.jpg",
+    desc: `You can find countless different variations main course and enjoy eating. `,
+  },
+
+  {
+    id: 23,
+    title: "Cool Drink",
+    category: "drink",
+    price: 6.99,
+    img: "./images/pic23.jpg",
+    desc: `You can find countless different variations on drinks, alcoholic and non-alcoholic drinks. `,
+  },
+
+  {
+    id: 24,
+    title: "Cool Drink",
+    category: "drink",
+    price: 6.99,
+    img: "./images/pic24.jpg",
+    desc: `You can find countless different variations on drinks, alcoholic and non-alcoholic drinks. `,
+  },
+  {
+    id: 25,
+    title: "Cool Drink",
+    category: "drink",
+    price: 6.99,
+    img: "./images/pic25.jpg",
+    desc: `You can find countless different variations on drinks, alcoholic and non-alcoholic drinks. `,
+  },
+  {
+    id: 26,
+    title: "Cool Drink",
+    category: "drink",
+    price: 6.99,
+    img: "./images/pic26.jpg",
+    desc: `You can find countless different variations on drinks, alcoholic and non-alcoholic drinks. `,
+  },
+  {
+    id: 27,
+    title: "Cool Drink",
+    category: "drink",
+    price: 6.99,
+    img: "./images/pic27.jpg",
+    desc: `You can find countless different variations on drinks, alcoholic and non-alcoholic drinks. `,
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+];
+export default menu;
